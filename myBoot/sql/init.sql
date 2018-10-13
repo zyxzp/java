1854db33b6da6f40ac12d7ad712784326a638ee0
zyx <1113470466@qq.com>
2018-10-13 16:47:35 +0800

    sql 商品表增加 状态字段

--- myBoot/sql/init.sql
+++ myBoot/sql/init.sql
@@ -6,6 +6,7 @@ create table `product_info`(
 `product_stock` int not null comment '库存',
 `product_description` varchar(64)  comment '描述',
 `product_icon` varchar(512)  comment '小图',
+`product_status` int not null comment '商品状态，0正常，1下架',
 `category_type` int not null comment '类目编号',
 `create_time` timestamp not null default  current_timestamp comment '创建时间',
 `update_time` timestamp not null default  current_timestamp on update current_timestamp comment '更新时间',
